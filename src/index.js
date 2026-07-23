ab4fa9e17a62ddde88773ce87f6c7e623fcefa0c
Cosmo <240465688+CosmoWeb@users.noreply.github.com>
2026-07-23 10:21:56 +0300

    Added two new functions: one to celar the content id the div content; one to add an event listeners to the buttins which clear the content and display the related content.

--- src/index.js
+++ src/index.js
@@ -2,4 +2,24 @@ import "./style.css";
 import {displayHome} from "./home.js";
 import {displayMenu} from "./menu.js";
 
-displayMenu();
+const cleanPage = () => {
+    const content = document.querySelector("#content");
+    content.textContent = "";
+};
+
+const displayPage = () => {
+    const buttons = document.querySelector("nav");
+    buttons.addEventListener("click", (event) => {
+        const target = event.target;
+        if(target.id === "home"){
+            cleanPage();
+            displayHome();
+        }
+        else if(target.id === "menu"){
+            cleanPage();
+            displayMenu();
+        }
+    });
+};
+
+displayPage();
